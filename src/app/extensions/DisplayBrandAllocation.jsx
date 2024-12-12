196bd0e719df8fc13cfbfe61ebb23e8164f2951d
Boazschilman <boaz.s@commbox.io>
2024-12-12 15:15:40 +0200

    Create DisplayBrandAllocation.jsx

---
+++ src/app/extensions/DisplayBrandAllocation.jsx
@@ -0,0 +1,23 @@
+{
+  "type": "crm-card",
+  "data": {
+    "title": "Brand Allocation",
+    "uid": "brand_allocation_card",
+    "location": "deals",
+    "objectTypes": [
+      { "name": "deals" }
+    ],
+    "module": {
+      "file": "DisplayBrandAllocation.jsx"
+    },
+    "actions": [
+      {
+        "type": "IFRAME",
+        "width": 600,
+        "height": 500,
+        "uri": "https://hubspot-brands.onrender.com/allocation-form",
+        "label": "Allocate Brands"
+      }
+    ]
+  }
+}

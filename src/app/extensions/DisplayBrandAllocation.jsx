0fb3d6a88a054985cf93f9c9821c460536ffa9b6
Boazschilman <boaz.s@commbox.io>
2024-12-12 15:42:21 +0200

    Update DisplayBrandAllocation.jsx

--- src/app/extensions/DisplayBrandAllocation.jsx
+++ src/app/extensions/DisplayBrandAllocation.jsx
@@ -1,32 +1,21 @@
-const { Link, Button, Text, Box, Flex, hubspot } = require("@hubspot/ui-extensions");
+const { Button, Box, Text, hubspot } = require("@hubspot/ui-extensions");
 
-hubspot.extend((context) => {
-  const Extension = ({ openIframe }) => {
-    const handleClick = () => {
-      openIframe({
-        uri: "https://hubspot-brands.onrender.com/allocation-form",
-        height: 600,
-        width: 500,
-        title: 'Brand Allocation',
-        flush: true,
-      });
-    };
-
-    return (
-      <Box>
-        <Flex direction="column" align="start" gap="medium">
-          <Text>
-            Click the button below to allocate agents between brands.
-          </Text>
-          <Box>
-            <Button type="submit" onClick={handleClick}>
-              Allocate Brands
-            </Button>
-          </Box>
-        </Flex>
-      </Box>
-    );
+hubspot.extend(({ actions }) => {
+  const handleClick = () => {
+    actions.openIframeModal({
+      uri: "https://hubspot-brands.onrender.com/allocation-form",
+      height: 600,
+      width: 500,
+      title: 'Brand Allocation'
+    });
   };
 
-  return <Extension openIframe={context.actions.openIframeModal} />;
+  return (
+    <Box>
+      <Text>Click to allocate agents between brands</Text>
+      <Button onClick={handleClick}>
+        Allocate Brands
+      </Button>
+    </Box>
+  );
 });

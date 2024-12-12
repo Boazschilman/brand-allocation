721c1e244389d16f40290bce823187af7e8fbf8d
Boazschilman <boaz.s@commbox.io>
2024-12-12 15:48:02 +0200

    Update DisplayBrandAllocation.jsx

--- src/app/extensions/DisplayBrandAllocation.jsx
+++ src/app/extensions/DisplayBrandAllocation.jsx
@@ -1,21 +1,9 @@
-const { Button, Box, Text, hubspot } = require("@hubspot/ui-extensions");
+const { Button, hubspot } = require("@hubspot/ui-extensions");
 
-hubspot.extend(({ actions }) => {
-  const handleClick = () => {
-    actions.openIframeModal({
-      uri: "https://hubspot-brands.onrender.com/allocation-form",
-      height: 600,
-      width: 500,
-      title: 'Brand Allocation'
-    });
-  };
-
+hubspot.extend(() => {
   return (
-    <Box>
-      <Text>Click to allocate agents between brands</Text>
-      <Button onClick={handleClick}>
-        Allocate Brands
-      </Button>
-    </Box>
+    <Button>
+      Allocate Brands
+    </Button>
   );
 });

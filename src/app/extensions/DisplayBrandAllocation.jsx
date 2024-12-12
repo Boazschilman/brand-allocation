fab200324aaecbfa752c644812f6a961afb6b273
Boazschilman <boaz.s@commbox.io>
2024-12-12 15:51:43 +0200

    Update DisplayBrandAllocation.jsx

--- src/app/extensions/DisplayBrandAllocation.jsx
+++ src/app/extensions/DisplayBrandAllocation.jsx
@@ -1,9 +1,9 @@
-// File: src/app/extensions/DisplayBrandAllocation.jsx
-'use strict';
-
 const { Button } = require('@hubspot/ui-extensions');
-const React = require('react');
 
-exports.extension = ({ actions }) => {
-  return React.createElement(Button, null, "Allocate Brands");
+exports.main = ({ context }) => {
+  return (
+    <Button>
+      Allocate Brands
+    </Button>
+  );
 };
